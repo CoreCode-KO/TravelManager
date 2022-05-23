a9f57dfc6c4e1c8cbbcea19616ecf33ec5594fdd
Karol Ostrowski <xlolsonx@gmail.com>
2022-05-23 13:09:42 +0200

    Update cors.rb

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -4,7 +4,7 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-
+# rubocop:disable Metrics/BlockLength
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
@@ -14,3 +14,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+# rubocop:enable Metrics/BlockLength
